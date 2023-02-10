1caecf35a2d0ee81cedea40ccbd4ad4150bca2f8
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2023-02-10 11:59:32 +0530

    Some minor changes.

--- src/scripts/optimize.ts
+++ src/scripts/optimize.ts
@@ -1,25 +1,19 @@
-let replace: (any[] | undefined)[] = [
+const replace: (any[] | undefined)[] = [
 	["head > script:nth-child(34)", { src: "" }],
-	["head > script:nth-child(35)", { src: "https://code.jquery.com/jquery-1.12.4.min.js", crossorigin: "anonymous" }],
 	["head > script:nth-child(43)", { src: "" }]
 ];
 let count = 0;
-let notDone = true;
 
 const observer = new MutationObserver(() => {
-	if (notDone) {
-		const meta = document.querySelector("head > meta:nth-child(2)");
-		if (meta) {
-			meta.setAttribute("http-equiv", "Content-Security-Policy");
-			meta.setAttribute("content", "default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *");
-			notDone = false;
-		}
-	}
 	for (let i = 0; i < replace.length; i++) {
 		if (replace[i]) {
 			const element = document.querySelector(replace[i]![0]) as HTMLElement | null;
 			if (element) {
 				const attributes = replace[i]![1];
+				if (attributes.innerHTML) {
+					element.innerHTML = attributes.innerHTML;
+				}
+				attributes.innerHTML = undefined;
 				for (const attribute in attributes) {
 					element.setAttribute(attribute, attributes[attribute]);
 				}
